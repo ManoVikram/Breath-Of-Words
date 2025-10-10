7b23a1b18439bc4729c7b4996e3757f8aede85b3
Mano Vikram <manovik18@gmail.com>
2025-10-10 19:04:15 +0530

    Added the starter code for main.go

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// Connect to the Python gRPC server
+	connection, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
+	}
+	defer connection.Close()
+
+	// Set up the Gin server
+	server := gin.Default()
+
+	server.RedirectTrailingSlash = true
+
+	// Start the Gin server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(server.Run(":" + port))
+}
